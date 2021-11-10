7ffd6f59087e4a334979d6ea7abe714b7377819d
Amber Lilith O. da Silva <amber.lilith.ordone@gmail.com>
2021-11-10 18:57:00 -0300

    Iniciando projeto para testar implementação de API REST com Spring Boot

---
+++ src/main/java/br/com/amber/testandoapi/testandoApi/TestandoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.amber.testandoapi.testandoApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestandoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestandoApiApplication.class, args);
+	}
+
+}

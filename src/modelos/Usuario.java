c3fd973f27ed81ed6aeb1d6cb2210b5914bd26f5
Amber Lilith O. da Silva <amber.lilith.ordone@gmail.com>
2021-11-10 19:48:20 -0300

    Criado package modelos e classe Usuario

---
+++ src/modelos/Usuario.java
@@ -0,0 +1,37 @@
+package modelos;
+
+public class Usuario {
+    private String nome;
+    private String cpf;
+    private String dataNascimento;
+
+    public Usuario(String nome, String cpf, String dataNascimento) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+}
